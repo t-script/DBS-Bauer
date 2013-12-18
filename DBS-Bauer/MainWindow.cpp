390937f7e2db88f9b5c2081288ef6e1ad068d1cb
David Wolf <d.wolf@live.at>
2013-12-18 14:06:29 +0100

    Trigger für die Buttons hinzugefügt. .gitignore abgeändert.

--- DBS-Bauer/MainWindow.cpp
+++ DBS-Bauer/MainWindow.cpp
@@ -1,5 +1,6 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
@@ -11,4 +12,41 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::on_actionStall_triggered()
+{
+	qDebug() << "Stall pressed.\n";
+}
+
+void MainWindow::on_actionAngestellter_triggered()
+{
+}
+
+void MainWindow::on_actionTier_triggered()
+{
+}
+
+void MainWindow::on_actionLager_triggered()
+{
+}
+
+void MainWindow::on_actionFutter_triggered()
+{
+}
+
+void MainWindow::on_actionMaschinen_triggered()
+{
+}
+
+void MainWindow::on_actionD_nger_triggered()
+{
+}
+
+void MainWindow::on_actionAcker_triggered()
+{
+}
+
+void MainWindow::on_actionSaatgut_triggered()
+{
 }
