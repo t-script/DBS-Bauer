9709b094ee7c35c94394922bcd7fe702d54e65bf
David Wolf <d.wolf@live.at>
2013-12-30 12:19:19 +0100

    Testdaten

--- 2. CreateTables.sql
+++ 2. CreateTables.sql
@@ -1,37 +1,40 @@
+-- drop schema public cascade;
+-- create schema public;
+
 CREATE TABLE STALL (
 	PK_Stall		serial PRIMARY KEY,
 	Stallart		text,
-	Kapazitaet		text NOT NULL,
-	Standort		text NOT NULL
+	Kapazitaet		text NOT NULL, -- gesamtkapazität tiere
+	Standort		text NOT NULL -- relative angabe mit referenz auf haupthof oder absolute angabe nach offiziellen ortsangaben
 );
 
 CREATE TABLE FUTTER (
 	PK_Futter		serial PRIMARY KEY,
 	Name			text,
-	Preis			money
+	Preis			float -- €/kg
 );
 
 CREATE TABLE DUENGER (
 	PK_DUEnger		serial PRIMARY KEY,
 	Name			text,
-	Preis			money
+	Preis			float -- €/kg
 );
 
 CREATE TABLE SAATGUT (
 	PK_Saatgut		serial PRIMARY KEY,
 	Name			text,
-	Preis			money
+	Preis			float -- €/kg
 );
 
 CREATE TABLE LAGER (
 	PK_Lager		serial PRIMARY KEY,
 	Lagerart		text,
-	Kapazitaet		text
+	Kapazitaet		integer -- kg
 );
 
 CREATE TABLE ACKER (
 	PK_Acker		serial PRIMARY KEY,
-	Groesse			integer
+	Groesse			integer -- m²
 );
 
 CREATE TYPE geschlecht AS ENUM ('maennlich' , 'weiblich');
@@ -40,7 +43,7 @@ CREATE TABLE ANGESTELLTER (
 	Vorname			text NOT NULL,
 	Nachname		text NOT NULL,
 	SVN			text NOT NULL,
-	Gehalt			money NOT NULL,
+	Gehalt			float NOT NULL,
 	Bankdaten		text,
         Geschlecht		geschlecht
 );
@@ -48,9 +51,9 @@ CREATE TABLE ANGESTELLTER (
 CREATE TABLE MASCHINE (
 	PK_Maschine		serial PRIMARY KEY,
         FK_Lager		serial REFERENCES LAGER (PK_Lager),
-	Kosten			money,
-	Abschreibungsdatum	date,
+	Kosten			float, -- €
 	Anschaffungsdatum	date,
+	Abschreibungsdatum	date,
 	Typ			text,
 	Name			text
 );
@@ -58,16 +61,16 @@ CREATE TABLE MASCHINE (
 CREATE TABLE TIER (
 	PK_Tier			serial PRIMARY KEY,
 	FK_Stall		serial REFERENCES STALL (PK_Stall),
-	Name			text,
+	Name			text, -- name des tieres (falls es überhaupt einen hat) -- bsp. Berta
 	Geburtsdatum		date NOT NULL,
 	Anschaffungs_Datum	date NOT NULL,
-	Gewicht			float
+	Gewicht			float -- kg
 );
 
 CREATE TABLE ATTRIBUTE (
 	PK_Attribute		serial PRIMARY KEY,
-	Name			text,
-	Wert			text
+	Name			text, -- bsp.: Tierart
+	Wert			text -- bsp.: Hausschwein
 );
 
 CREATE TABLE TIER_ATTRIBUTE (
@@ -77,7 +80,7 @@ CREATE TABLE TIER_ATTRIBUTE (
 
 CREATE TABLE TIERARZTBESUCH (
 	PK_Tierarztbesuch	serial PRIMARY KEY,
-	FK_Tier			serial NOT NULL,
+	FK_Tier			serial REFERENCES TIER (PK_Tier),
 	Datum			date,
 	Diagnose		text,
 	Medikamente		text
@@ -86,8 +89,8 @@ CREATE TABLE TIERARZTBESUCH (
 CREATE TABLE FUTTERMENGE_PRO_TIER (
         FK_Tier			serial REFERENCES TIER (PK_Tier),
         FK_Futter		serial REFERENCES FUTTER (PK_Futter),
-	Datum_von		date,
-	Datum_bis		date
+        Menge			float NOT NULL, -- kg/day
+	Datum			date DEFAULT now()
 );
 
 CREATE TABLE FUTTER_BESTAND (
@@ -112,7 +115,7 @@ CREATE TABLE ACKERDATEN (
         FK_Acker		serial REFERENCES ACKER (PK_Acker),
         FK_Duenger		serial REFERENCES DUENGER (PK_Duenger),
         FK_Saatgut		serial REFERENCES SAATGUT (PK_Saatgut),
-	Datum			date,
+	Datum			date, -- datum der aussaat
 	Messwert		float
 );
 
@@ -120,7 +123,7 @@ CREATE TABLE MASCHINE_VERWENDUNG (
         FK_Angestellter		serial REFERENCES ANGESTELLTER (PK_Angestellter),
         FK_Maschine		serial REFERENCES MASCHINE (PK_Maschine),
 	Verwendungsdatum	date,
-	Dauer			integer
+	Dauer			float -- h
 );
 
 CREATE TABLE ANGESTELLTER_STALLARBEITEN (
@@ -128,6 +131,6 @@ CREATE TABLE ANGESTELLTER_STALLARBEITEN (
         FK_Angestellter		serial REFERENCES ANGESTELLTER (PK_Angestellter),
 	VerichteteArbeit	text,
 	Datum			date,
-	Dauer			integer
+	Dauer			float -- h
 );
 
