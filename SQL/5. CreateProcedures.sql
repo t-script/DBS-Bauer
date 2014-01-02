636ebc93da8ca58dc836038095bc942acd8d124c
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-01-02 23:35:09 +0100

    Funktion hinzugefügt, Tierarztbesuche pro Tier

---
+++ SQL/5. CreateProcedures.sql
@@ -0,0 +1,30 @@
+﻿CREATE OR REPLACE FUNCTION usp_TierArztBesuche(_id integer) 
+	RETURNS TABLE(PK_Tier integer, Geburtsdatum date, Gewicht double precision, "Alter" interval, Datum date, Diagnose text, Medikamente text ) AS $func$
+DECLARE
+
+BEGIN
+
+RETURN QUERY EXECUTE 
+	'
+	SELECT  
+		TIER.PK_Tier,
+		TIER.Geburtsdatum,
+		TIER.Gewicht,
+		(now() - TIER.Anschaffungs_Datum) AS Alter,
+		TIERARZTBESUCH.Datum,
+		TIERARZTBESUCH.Diagnose,
+		TIERARZTBESUCH.Medikamente
+	FROM 
+		TIERARZTBESUCH
+	JOIN 
+		TIER
+	ON
+		TIERARZTBESUCH.FK_Tier = TIER.PK_Tier
+	WHERE	
+		TIER.PK_Tier = $1'
+USING _id;
+
+END
+$func$ LANGUAGE plpgsql;
+
+--SELECT * FROM usp_TierArztBesuche(2002);
