05458fa3abcaa324961195cf5be4de1a15675744
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-01-03 22:40:07 +0100

    usp_MaschinenVerwendung hinzugefügt

--- SQL/5. CreateProcedures.sql
+++ SQL/5. CreateProcedures.sql
@@ -32,7 +32,9 @@ RETURN QUERY
 END
 $func$ LANGUAGE plpgsql;
 
-COMMENT ON FUNCTION usp_TierArztBesuche(_id integer) IS 'Zeigt alle Tierarztbesuche des Tieres anhand des Primary Keys an';
+COMMENT ON 
+	FUNCTION usp_TierArztBesuche(_id integer) 
+	IS 'Zeigt alle Tierarztbesuche des Tieres anhand des Primary Keys an';
 --SELECT * FROM usp_TierArztBesuche(1023);
 
 CREATE OR REPLACE FUNCTION usp_AngestellterStallarbeiten(_id integer, _start date, _end Date) 
@@ -67,9 +69,58 @@ RETURN QUERY
 		ANGESTELLTER_STALLARBEITEN.FK_Angestellter = ANGESTELLTER.PK_Angestellter
 	WHERE
 		ANGESTELLTER.PK_Angestellter = _id AND
-		ANGESTELLTER_STALLARBEITEN.Datum BETween _start and _end;
+		ANGESTELLTER_STALLARBEITEN.Datum BETWEEN _start and _end;
 end
 $func$ language plpgsql;
 
-COMMENT ON FUNCTION usp_AngestellterStallarbeiten(_id integer, _start date, _end Date) IS 'Gibt alle Verrichteten Arbeiten eines Angestellten innerhalb des angegebenenZeitraums aus';
+COMMENT ON 
+	FUNCTION usp_AngestellterStallarbeiten(_id integer, _start date, _end Date) 
+	IS 'Gibt alle Verrichteten Arbeiten eines Angestellten innerhalb des angegebenen Zeitraums aus';
 --SELECT * FROM usp_AngestellterStallarbeiten(2, '1961-06-16', now()::date);
+
+CREATE OR REPLACE FUNCTION usp_MaschineVerwendung(_id integer, _start date, _end Date)
+RETURNS TABLE(
+		PK_Angestellter integer,
+		Vorname text,
+		Nachname text,
+		FK_Maschine integer,
+		Datum date,
+		Dauer interval,
+		Typ text,
+		name text
+	)
+	AS $func$
+declare
+
+BEGIN
+
+RETURN QUERY
+	SELECT
+		ANGESTELLTER.PK_Angestellter,
+		ANGESTELLTER.Vorname,
+		ANGESTELLTER.Nachname,
+		MASCHINE_VERWENDUNG.FK_Maschine,
+		MASCHINE_VERWENDUNG.Verwendungsdatum,
+		MASCHINE_VERWENDUNG.Dauer,
+		MASCHINE.Typ,
+		MASCHINE.Name
+	FROM
+		public.ANGESTELLTER
+	JOIN
+		public.MASCHINE_VERWENDUNG
+	ON
+		MASCHINE_VERWENDUNG.FK_Angestellter = ANGESTELLTER.PK_Angestellter
+	JOIN
+		public.MASCHINE
+	ON
+		MASCHINE_VERWENDUNG.FK_Maschine = MASCHINE.PK_Maschine
+	WHERE
+		ANGESTELLTER.PK_Angestellter = _id AND
+		MASCHINE_VERWENDUNG.Verwendungsdatum BETWEEN _start and _end;
+end
+$func$ language plpgsql;
+
+COMMENT ON
+	FUNCTION usp_MaschineVerwendung(_id integer, _start date, _end Date)
+	IS 'Gibt alle Verwendeten Maschinen eines Angestellten innerhalb des angegebenen Zeitraums aus';
+--SELECT * FROM usp_MaschineVerwendung(2, '1961-06-16', now()::date);
