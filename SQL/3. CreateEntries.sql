cf05faa1c8f5adc1e24442ffbea2da757384d9c0
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-01-04 01:56:31 +0100

    Händisches Eintragen von PK verhindert SEQ update

--- SQL/3. CreateEntries.sql
+++ SQL/3. CreateEntries.sql
@@ -7,73 +7,73 @@ TRUNCATE DUENGER CASCADE;
 TRUNCATE ATTRIBUTE CASCADE;
 TRUNCATE SAATGUT CASCADE;
 
-INSERT INTO STALL VALUES (1, 'Schweinestall', '1000', 'Nebenhof');
-INSERT INTO STALL VALUES (2, 'Kuhstall', '1000', 'Haupthof');
-INSERT INTO STALL VALUES (3, 'Hühnerstall', '1000', 'Haupthof');
+INSERT INTO STALL(Stallart, Kapazitaet, Standort) VALUES ('Schweinestall', '1000', 'Nebenhof');
+INSERT INTO STALL(Stallart, Kapazitaet, Standort) VALUES ('Kuhstall', '1000', 'Haupthof');
+INSERT INTO STALL(Stallart, Kapazitaet, Standort) VALUES ('Hühnerstall', '1000', 'Haupthof');
 
-INSERT INTO ATTRIBUTE VALUES (1, 'Tierart', 'Hausschwein');
-INSERT INTO ATTRIBUTE VALUES (2, 'Tierart', 'Hausrind');
-INSERT INTO ATTRIBUTE VALUES (3, 'Tierart', 'Haushuhn');
+INSERT INTO ATTRIBUTE(Name, Wert) VALUES ('Tierart', 'Hausschwein');
+INSERT INTO ATTRIBUTE(Name, Wert) VALUES ('Tierart', 'Hausrind');
+INSERT INTO ATTRIBUTE(Name, Wert) VALUES ('Tierart', 'Haushuhn');
 
-INSERT INTO FUTTER VALUES (1, 'Medizinfutter', 10);
-INSERT INTO FUTTER VALUES (2, 'Wachtumsfutter', 5);
-INSERT INTO FUTTER VALUES (3, 'Hühnerfutter', 1);
-INSERT INTO FUTTER VALUES (4, 'Heu', 0.5);
+INSERT INTO FUTTER(Name, Preis) VALUES ('Medizinfutter', 10);
+INSERT INTO FUTTER(Name, Preis) VALUES ('Wachtumsfutter', 5);
+INSERT INTO FUTTER(Name, Preis) VALUES ('Hühnerfutter', 1);
+INSERT INTO FUTTER(Name, Preis) VALUES ('Heu', 0.5);
 
-INSERT INTO DUENGER VALUES (1, 'Schweinedung', 0);
-INSERT INTO DUENGER VALUES (2, 'Kuhdung', 0);
-INSERT INTO DUENGER VALUES (3, 'Pestizid', 12);
-INSERT INTO DUENGER VALUES (4, 'Wachstumsdünger', 5);
+INSERT INTO DUENGER(Name, Preis) VALUES ('Schweinedung', 0);
+INSERT INTO DUENGER(Name, Preis) VALUES ('Kuhdung', 0);
+INSERT INTO DUENGER(Name, Preis) VALUES ('Pestizid', 12);
+INSERT INTO DUENGER(Name, Preis) VALUES ('Wachstumsdünger', 5);
 
-INSERT INTO SAATGUT VALUES (1, 'Mais', 3);
-INSERT INTO SAATGUT VALUES (2, 'Genmais', 1.5);
-INSERT INTO SAATGUT VALUES (3, 'Getreide', 1);
-INSERT INTO SAATGUT VALUES (4, 'Karotten', 2);
-INSERT INTO SAATGUT VALUES (5, 'Zuckerrübe', 3);
-INSERT INTO SAATGUT VALUES (6, 'Erdäpfel', 2.5);
+INSERT INTO SAATGUT(Name, Preis) VALUES ('Mais', 3);
+INSERT INTO SAATGUT(Name, Preis) VALUES ('Genmais', 1.5);
+INSERT INTO SAATGUT(Name, Preis) VALUES ('Getreide', 1);
+INSERT INTO SAATGUT(Name, Preis) VALUES ('Karotten', 2);
+INSERT INTO SAATGUT(Name, Preis) VALUES ('Zuckerrübe', 3);
+INSERT INTO SAATGUT(Name, Preis) VALUES ('Erdäpfel', 2.5);
 
-INSERT INTO LAGER VALUES (1, 'Silo', 100000);
-INSERT INTO LAGER VALUES (2, 'Dachspeicher', 500);
-INSERT INTO LAGER VALUES (3, 'Scheune', 100);
-INSERT INTO LAGER VALUES (4, 'Großspeicher', 5000);
-INSERT INTO LAGER VALUES (5, 'Saatsilo', 1500);
+INSERT INTO LAGER(Lagerart, Kapazitaet) VALUES ('Silo', 100000);
+INSERT INTO LAGER(Lagerart, Kapazitaet) VALUES ('Dachspeicher', 500);
+INSERT INTO LAGER(Lagerart, Kapazitaet) VALUES ('Scheune', 100);
+INSERT INTO LAGER(Lagerart, Kapazitaet) VALUES ('Großspeicher', 5000);
+INSERT INTO LAGER(Lagerart, Kapazitaet) VALUES ('Saatsilo', 1500);
 
 DO $$
 
 BEGIN
 -- Schweine
 	FOR i IN 1..1001 LOOP
-		INSERT INTO TIER VALUES(i, 1, null, now(), now(), (i + 200) % 300);
-		INSERT INTO TIER_ATTRIBUTE VALUES(i, 1);
-		INSERT INTO FUTTERMENGE_PRO_TIER VALUES(i, 2, 5);
+		INSERT INTO TIER(FK_Stall, Name, Geburtsdatum,Anschaffungs_Datum, Gewicht) VALUES(1, null, now(), now(), (i + 200) % 300);
+		INSERT INTO TIER_ATTRIBUTE VALUES(currval('tier_pk_tier_seq'), 1);
+		INSERT INTO FUTTERMENGE_PRO_TIER VALUES(currval('tier_pk_tier_seq'), 2, 5);
 	END LOOP;
 -- Rinder
 	FOR i IN 1002..2002 LOOP
-		INSERT INTO TIER VALUES(i, 2, null, now(), now(), (i + 300) % 500);
-		INSERT INTO TIER_ATTRIBUTE VALUES(i, 2);
-		INSERT INTO FUTTERMENGE_PRO_TIER VALUES(i, 2, 3);
-		INSERT INTO FUTTERMENGE_PRO_TIER VALUES(i, 4, 3);
-		INSERT INTO TIERARZTBESUCH(fk_tier,datum, diagnose, medikamente) VALUES (i, current_date + i % 10, null, null);
+		INSERT INTO TIER(FK_Stall, Name, Geburtsdatum,Anschaffungs_Datum, Gewicht) VALUES(2, null, now(), now(), (i + 300) % 500);
+		INSERT INTO TIER_ATTRIBUTE VALUES(currval('tier_pk_tier_seq'), 2);
+		INSERT INTO FUTTERMENGE_PRO_TIER VALUES(currval('tier_pk_tier_seq'), 2, 3);
+		INSERT INTO FUTTERMENGE_PRO_TIER VALUES(currval('tier_pk_tier_seq'), 4, 3);
+		INSERT INTO TIERARZTBESUCH(FK_Tier,Datum, Diagnose, Medikamente) VALUES (currval('tier_pk_tier_seq'), current_date + i % 10, null, null);
 	END LOOP;
 -- Hühner
 	FOR i IN 2003..3003 LOOP
-		INSERT INTO TIER VALUES(i, 3, null, now(), now(), i % 3);
-		INSERT INTO TIER_ATTRIBUTE VALUES(i, 3);
-		INSERT INTO FUTTERMENGE_PRO_TIER VALUES(i, 3, 1.5);
+		INSERT INTO TIER(FK_Stall, Name, Geburtsdatum,Anschaffungs_Datum, Gewicht) VALUES(3, null, now(), now(), i % 3);
+		INSERT INTO TIER_ATTRIBUTE VALUES(currval('tier_pk_tier_seq'), 3);
+		INSERT INTO FUTTERMENGE_PRO_TIER VALUES(currval('tier_pk_tier_seq'), 3, 1.5);
 	END LOOP;
 END$$;
 
 --SELECT * FROM TIER;
 --TRUNCATE STALL CASCADE;
 
-INSERT INTO ANGESTELLTER VALUES (1, 'Bartholomeus', 'Kufstein', '2211010203', 1500.75, null, 'maennlich');
-INSERT INTO ANGESTELLTER VALUES (2, 'Lothar', 'Semmering', '3322020304', 2750.25, null, 'maennlich');
-INSERT INTO ANGESTELLTER VALUES (3, 'Gertrude', 'Pommern', '4433030405', 2500.50, null, 'weiblich');
-INSERT INTO ANGESTELLTER VALUES (4, 'Kunigunde', 'Holstein', '5544040506', 1750.25, null, 'weiblich');
+INSERT INTO ANGESTELLTER(Vorname, Nachname, SVN, Gehalt, Bankdaten, Geschlecht) VALUES ('Bartholomeus', 'Kufstein', '2211010203', 1500.75, null, 'maennlich');
+INSERT INTO ANGESTELLTER(Vorname, Nachname, SVN, Gehalt, Bankdaten, Geschlecht) VALUES ('Lothar', 'Semmering', '3322020304', 2750.25, null, 'maennlich');
+INSERT INTO ANGESTELLTER(Vorname, Nachname, SVN, Gehalt, Bankdaten, Geschlecht) VALUES ('Gertrude', 'Pommern', '4433030405', 2500.50, null, 'weiblich');
+INSERT INTO ANGESTELLTER(Vorname, Nachname, SVN, Gehalt, Bankdaten, Geschlecht) VALUES ('Kunigunde', 'Holstein', '5544040506', 1750.25, null, 'weiblich');
 
-INSERT INTO MASCHINE VALUES (1, 3, 75000.0, date '2012-01-01', date '2012-01-01' + 1825, 'Steyr Traktor', '6140 PROFI');
-INSERT INTO MASCHINE VALUES (2, 3, 97500.0, date '2012-03-02', date '2012-03-02' + 1825, 'Steyr Traktor', 'multi 4115');
-INSERT INTO MASCHINE VALUES (3, 3, 199500.0, date '2011-11-05', date '2011-11-05' + 1825, 'Fendt Mähdrescher', '9490 X AL');
+INSERT INTO MASCHINE(FK_Lager, Kosten, Anschaffungsdatum, Abschreibungsdatum, Typ, Name) VALUES (3, 75000.0, date '2012-01-01', date '2012-01-01' + 1825, 'Steyr Traktor', '6140 PROFI');
+INSERT INTO MASCHINE(FK_Lager, Kosten, Anschaffungsdatum, Abschreibungsdatum, Typ, Name) VALUES (3, 97500.0, date '2012-03-02', date '2012-03-02' + 1825, 'Steyr Traktor', 'multi 4115');
+INSERT INTO MASCHINE(FK_Lager, Kosten, Anschaffungsdatum, Abschreibungsdatum, Typ, Name) VALUES (3, 199500.0, date '2011-11-05', date '2011-11-05' + 1825, 'Fendt Mähdrescher', '9490 X AL');
 
 INSERT INTO FUTTER_BESTAND VALUES (1, 4, 200);
 INSERT INTO FUTTER_BESTAND VALUES (2, 4, 1000);
